6838ba66ec31c86e9818df3c029174b304ecf24e
Jeff Flower <jeffrey.flower@vantiv.com>
2017-02-15 10:04:01 -0700

    refactor dropdown listener

--- src/scripts/controllers/tableController.js
+++ src/scripts/controllers/tableController.js
@@ -16,6 +16,8 @@ export var tableExport = {
   tables: tables
 }; 
 
+
+
 /***** Public Functions *****/
 
 /**
@@ -51,6 +53,7 @@ function addTable(chartname, txnType) {
     setDrawFunc(chartname);
 
     // add dropdown listener
+    addDropdownListener(chartname);
 
     // draw table
     draw(chartname);
@@ -58,6 +61,28 @@ function addTable(chartname, txnType) {
 }
 
 /***** Private Functions *****/
+
+// this var for exporting functions to be tested only, not intended to be used in code
+export var testing = {
+  uniqueMccNames: uniqueMccNames,
+  buildTableData: buildTableData
+};
+
+/** 
+ * @function addDropdownListener
+ * @param {String} chartname - css selector for chart
+ */
+function addDropdownListener(chartname) {
+  // build listener target 
+  let selector = chartname + " .dropdown-menu li a";
+
+  // build callback function
+  let cb = dropdownCallbackBuilder(chartname);
+
+  // add listener
+  //"click.mine" prevents previous event listeners from being overwritten
+  d3.selectAll(selector).on('click.mine', cb);
+}
 
 /**
  * Add the table html structure(table, header and body) inside the table container for chartname 
@@ -156,104 +181,121 @@ function draw(chartname) {
 }
 
 /**
-* Get the first item in chartname's dropdown list
-* @private
-* @function getDropdownDefault
-* @param {String} chartname
-* @return {String} value of the first dropdown item
-*/
-function getDropdownDefault(chartname) {
-let selector = chartname + ' .dropdown-menu li a';
-let val = d3.select(selector).attr('data-value');
+ * @function dropdownCallbackBuilder
+ * @param {String} chartname - css selector for chart
+ */
+function dropdownCallbackBuilder(chartname) {
+  return function(d) {
+    // get selected dropdown value
+    let val = d3.select(this).attr('data-value');
 
-return val;
+    // set dropdown value
+    setDropdown(chartname, val);
+    
+    // draw table
+    draw(chartname);
+  }
+}
+
+/**
+ * Get the first item in chartname's dropdown list
+ * @private
+ * @function getDropdownDefault
+ * @param {String} chartname
+ * @return {String} value of the first dropdown item
+ */
+function getDropdownDefault(chartname) {
+  let selector = chartname + ' .dropdown-menu li a';
+  let val = d3.select(selector).attr('data-value');
+
+  return val;
 } 
 
 /**
-* @function handleError
-* @private
-* @param {Object} err - Error object with message property
-*/
+ * @function handleError
+ * @private
+ * @param {Object} err - Error object with message property
+ */
 function handleError(err) {
-console.log(err.message);
+  console.log(err.message);
 }
 
 /**
-* Set the data needed for the table 
-* @function buildData
-* @private
-* @param {String} chartname - css selector for chart
-* @param {String} txnType
-*/
+ * Set the data needed for the table 
+ * @function buildData
+ * @private
+ * @param {String} chartname - css selector for chart
+ * @param {String} txnType
+ */
 function setData(chartname, txnType) {
 
-let insightsData = getInsightsData(txnType); // result is object with keys for each fi and values of arrays of objects
+  let insightsData = getInsightsData(txnType); // result is object with keys for each fi and values of arrays of objects
 
-try {
-tableModel.setData(chartname, insightsData);
-}
-catch (e) {
-handleError(e.message);
-}
+  try {
+    tableModel.setData(chartname, insightsData);
+  }
+  catch (e) {
+    handleError(e.message);
+  }
 }
 
 /**
-* @function setDrawFunc
-* @private
-* @param {String} chartname
-*/
+ * @function setDrawFunc
+ * @private
+ * @param {String} chartname
+ */
 function setDrawFunc(chartname) {
-if(tables.hasOwnProperty(chartname)) {
-tables[chartname].drawFunc = tableChart();
-} else {
-throw new InvalidTableError(chartname);
-}
+  if(tables.hasOwnProperty(chartname)) {
+    tables[chartname].drawFunc = tableChart();
+  } else {
+    throw new InvalidTableError(chartname);
+  }
 }
 
 /**
-* Set the dropdown param of the associated chart
-* @function setDropdown
-* @private
-* @param {String} chartname - css selector for chart
-* @param {String} [val] - optional dropdown value
-*/
+ * Set the dropdown param of the associated chart
+ * @function setDropdown
+ * @private
+ * @param {String} chartname - css selector for chart
+ * @param {String} [val] - optional dropdown value
+ */
 function setDropdown(chartname, val) {
 
-// if user did not pass in val, default to first dropdown list element
-if(val === undefined) {
-val = getDropdownDefault(chartname);
-}
+  // if user did not pass in val, default to first dropdown list element
+  if(val === undefined) {
+    val = getDropdownDefault(chartname);
+  }
 
-try {
-tableModel.setDropdown(chartname, val);
-}
-catch (e) {
-handleError(e);
-}
+  try {
+    tableModel.setDropdown(chartname, val);
+  }
+  catch (e) {
+    handleError(e);
+  }
 }
 
 /**
-* Find the unique mcc_name properties
-* @private
-* @function uniqueMccNames
-* @param {Object} data - object belonging to transaction type in the model
-* @returns {string[]}
-*/
+ * Find the unique mcc_name properties
+ * @private
+ * @function uniqueMccNames
+ * @param {Object} data - object belonging to transaction type in the model
+ * @returns {string[]}
+ */
 function uniqueMccNames(data) {
-// for each fi, get all mcc_names, result is array of arrays
-var fiNames = Object.keys(data);
-var mccArrays = fiNames.map( (fi) => {
-var arr = data[fi];
-return arr.map( (obj) => {
-return obj.mcc_name; 
-});
-});
+  // for each fi, get all mcc_names, result is array of arrays
+  var fiNames = Object.keys(data);
+  var mccArrays = fiNames.map( (fi) => {
+    var arr = data[fi];
+    return arr.map( (obj) => {
+      return obj.mcc_name; 
+    });
+  });
 
-// join arrays to form single array
-var allMccNames = mccArrays.reduce( (res, current) => {
-return res.concat(current);
-}, []);
+  // join arrays to form single array
+  var allMccNames = mccArrays.reduce( (res, current) => {
+    return res.concat(current);
+  }, []);
 
-// remove repeated values 
-return Array.from(new Set(allMccNames)); 
+  // remove repeated values 
+  return Array.from(new Set(allMccNames)); 
 }
